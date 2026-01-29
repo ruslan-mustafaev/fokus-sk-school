26c5f75137418eebb28e5fcd5eed1cd4debec8c8
Graycheg <graycheg@gmail.com>
2026-01-29 21:34:07 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
-import ForWhoSection from './components/ForWhoSection';
 import FormatsSection from './components/FormatsSection';
 import PricingSection from './components/PricingSection';
 import TeachersSection from './components/TeachersSection';
@@ -23,7 +22,6 @@ function App() {
       <Header onQuizClick={() => setShowQuiz(true)} />
       <HeroSection />
       <AboutSection />
-      <ForWhoSection />
       <FormatsSection onQuizClick={() => setShowQuiz(true)} />
       <PricingSection />
       <TeachersSection />
