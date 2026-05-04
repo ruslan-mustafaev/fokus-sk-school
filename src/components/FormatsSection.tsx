f9bc142c70e4304c14a097e208b3a2c7862ea3c6
ruslan-mustafaev <therox5551998@gmail.com>
2026-05-04 13:09:37 +0200

    Updated FormatsSection.tsx

--- src/components/FormatsSection.tsx
+++ src/components/FormatsSection.tsx
@@ -141,7 +141,7 @@ export default function FormatsSection({
           <AnimatedElement animation="fade-in-down">
             <h2 className="text-4xl md:text-5xl lg:text-6xl font-black mb-6 text-white uppercase tracking-wide">
               Обери свій{" "}
-              <span className="text-brand-orange">Формат</span>{" "}
+              <span className="text-brand-orange">формат</span>{" "}
               навчання
             </h2>
           </AnimatedElement>
