93571904366e2d75c822aa40b5424d2e5b21477a
ruslan-mustafaev <therox5551998@gmail.com>
2026-05-21 12:03:55 +0200

    Updated HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
         id="hero"
         className="relative min-h-screen flex flex-col justify-end"
       >
-        <div className="absolute inset-0 bg-indigo-950/35" />
+        <div className="absolute inset-0 bg-indigo-950/55" />
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 w-full pb-16 md:pb-20">
           <div className="max-w-3xl">
             <AnimatedElement animation="fade-in-up" delay={100}>
